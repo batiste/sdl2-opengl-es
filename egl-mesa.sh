6ad0f0b3b0c708b815f3a1c485545b9f3837cd69
Batiste Bieler <batiste.bieler@gmail.com>
2012-05-07 14:47:27 +0200

    Cleanup

--- egl-mesa.sh
+++
@@ -1,4 +0,0 @@
-wget ftp://ftp.freedesktop.org/pub/mesa/8.0.2/MesaLib-8.0.2.tar.bz2
-tar -xvf MesaLib-8.0.2.tar.bz2
-rm MesaLib-8.0.2.tar.bz2
-apt-get install llvm-dev libxcb-glx0-dev libx11-xcb-dev libxdamage-dev xdamage x11proto-dri2-dev x11proto-gl-dev bison flex xutils-dev
